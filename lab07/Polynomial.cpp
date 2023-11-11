5b6247e1e4183d39882d5306e19d85cb7efbbf71
dezsokee <dezsoszabolcs46@gmail.com>
2023-11-11 14:42:34 +0200

    Polynomial exercises done

--- lab07/Polynomial.cpp
+++ lab07/Polynomial.cpp
@@ -1,8 +1,13 @@
 #include "Polynomial.h"
+#include <cmath>
 
 Polynomial::Polynomial(int degree, const double *coefficients) {
     this->capacity = degree;
     this->coefficients = new double[degree + 1];
+
+    for (int i = 0; i < degree + 1; ++i) {
+        this->coefficients[i] = coefficients[i];
+    }
 }
 
 Polynomial::Polynomial(const Polynomial &that) {
@@ -38,6 +43,90 @@ double Polynomial::evaluate(double x) const {
     }
 
     return result;
+}
+
+Polynomial Polynomial::derivative() const {
+    Polynomial result(this->capacity - 1, this->coefficients);
+
+    for (int i = 0; i < this->capacity; ++i) {
+        result.coefficients[i] = this->coefficients[i + 1] * (i + 1);
+    }
+
+    return result;
+}
+
+double Polynomial::operator[](int index) const {
+    return this->coefficients[index];
+}
+
+Polynomial operator-(const Polynomial &a, const Polynomial &b) {
+
+    if (a.capacity > b.capacity) {
+        Polynomial result(a.capacity, a.coefficients);
+
+        for (int i = 0; i < result.capacity + 1; ++i) {
+            result.coefficients[i] = a.coefficients[i] - b.coefficients[i];
+        }
+
+        return result;
+    } else {
+        Polynomial result(b.capacity, b.coefficients);
+
+        for (int i = 0; i < result.capacity + 1; ++i) {
+            result.coefficients[i] = a.coefficients[i] - b.coefficients[i];
+        }
+
+        return result;
+    }
+}
+
+Polynomial operator+(const Polynomial &a, const Polynomial &b) {
+
+        if (a.capacity > b.capacity) {
+            Polynomial result(a.capacity, a.coefficients);
+
+            for (int i = 0; i < result.capacity + 1; ++i) {
+                result.coefficients[i] = a.coefficients[i] + b.coefficients[i];
+            }
+
+            return result;
+        } else {
+            Polynomial result(b.capacity, b.coefficients);
+
+            for (int i = 0; i < result.capacity + 1; ++i) {
+                result.coefficients[i] = a.coefficients[i] + b.coefficients[i];
+            }
+
+            return result;
+        }
+}
+
+Polynomial operator-(const Polynomial &a) {
+    Polynomial result(a.capacity, a.coefficients);
+
+    for (int i = 0; i < result.capacity + 1; ++i) {
+        result.coefficients[i] = -a.coefficients[i];
+    }
+
+    return result;
+}
+
+Polynomial operator*(const Polynomial &a, const Polynomial &b) {
+    Polynomial result(a.capacity + b.capacity, a.coefficients);
+
+    for (int i = 0; i < a.capacity + 1; ++i) {
+        result.coefficients[i] = a.coefficients[i] * b.coefficients[i];
+    }
+
+    return result;
+}
+
+ostream &operator<<(ostream &out, const Polynomial &what) {
+    for (int i = 0; i < what.capacity + 1; ++i) {
+        out << what.coefficients[i] << "x^" << i << " ";
+    }
+
+    return out;
 }
 
 
--- lab07/main_07.cpp
+++ lab07/main_07.cpp
@@ -1,8 +1,41 @@
 #include <iostream>
+#include "Polynomial.h"
 
 using namespace std;
 
 int main() {
+
+    Polynomial p1(3, new double[4]{1, 2, 3, 4});
+
+    cout << "p1: " << p1 << endl;
+
+    Polynomial p2(2, new double[3]{1, 2, 3});
+
+    cout << "p2: " <<  p2 << endl;
+
+    Polynomial p3 = p1 + p2;
+
+    cout << "p3 = p1 + p2 =  " << p3 << endl;
+
+    Polynomial p4 = p1 - p2;
+
+    cout << "p4 = p1 - p2 =  " << p4 << endl;
+
+    Polynomial p5 = p1 * p2;
+
+    cout << "p5 = p1 * p2 =  " << p5 << endl;
+
+    Polynomial p6 = p1.derivative();
+
+    cout << "p6 = p1.derivative() =  " << p6 << endl;
+
+    Polynomial p7 = -p1;
+
+    cout << "p7 = -p1 =  " << p7 << endl;
+
+    cout<<"p1[2] = "<<p1[2]<<endl;
+
+    cout<<"p1(2) = "<<p1.evaluate(2)<<endl;
 
     return 0;
 }
